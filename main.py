6ba5a4228851cd1b448dcef67d04a2ac1c02e199
daniemart <github email address>
2019-11-03 08:48:07 -0800

    got the randomizer to work and prints out word with letters eperated

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import random
 
-answerlist = ["world", "hello", "supercalifragilisticexpialidocious", "illuminati"]
-answer = random.shuffle(answerlist)
+answerlist = ["world", "hello", "supercalifragilisticexpialidocious", "illuminati", "turtle", "backpack"]
+# randomizes the word list
+random.shuffle(answerlist)
+# picks out the word and separates the letters
+answer = list(answerlist[0])
+
 print(answer)
 
+display = []
+display.extend(answer)
