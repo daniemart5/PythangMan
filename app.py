8c2f703117117238228cc951b3f1c565d8e7c44f
daniemart <github email address>
2019-11-10 20:34:25 -0800

    working with methods in python

--- app.py
+++ app.py
@@ -11,7 +11,28 @@
 # color = input("Whats your favorite color?")
 # print(name  + ' likes ' + color)
 
-birth_year = input("Birth year:")
-# int turns the input into an integer
-age = 2019 - int(birth_year)
-print(age)
+# birth_year = input("Birth year:")
+# # int turns the input into an integer
+# age = 2019 - int(birth_year)
+# print(age)
+
+# course = 'this is for beginners'
+# print(course[-1])
+# print(course[0:3])
+
+# first = 'John'
+# last = 'Smith'
+
+# msg = f"{first} [{last}] is a coder"
+# print(msg) 
+
+#note to self keep eye on indents in python
+
+#METHODS
+course = 'Bob is awesome'
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course.find('e'))
+print(course.replace('Bob', 'Danie'))
+
