d92130f602a8b883a7a4f7b71c50395c88127adc
daniemart <github email address>
2019-11-13 10:26:19 -0800

    created a weight converter with tut help

--- logicops.py
+++ logicops.py
@@ -1,17 +1,26 @@
 # if an applicant has good credit and good income
 
-has_good_income = True 
-has_good_credit = True
+# has_good_income = True 
+# has_good_credit = True
 
-if has_good_income and has_good_credit:
-    print("GIVE US YOUR MONEY")
-else:
-    print('YOU SUCK')
+# if has_good_income and has_good_credit:
+#     print("GIVE US YOUR MONEY")
+# else:
+#     print('YOU SUCK')
 
-has_good_income = False 
-has_good_credit = False
+# has_good_income = False 
+# has_good_credit = False
 
-if has_good_income or has_good_credit:
-    print("GIVE US YOUR MONEY")
-else:
-    print('YOU SUCK')
+# if has_good_income or has_good_credit:
+#     print("GIVE US YOUR MONEY")
+# else:
+#     print('YOU SUCK')
+
+# name = 'Jason Billy Andersonville the third of his kind and nature'
+
+# if len(name) < 3:
+#     print('Name must be more than 3 letters!')
+# elif len(name) > 50:
+#     print('Name is too long!')
+# else:
+#     print('Thank you, ' + name)
