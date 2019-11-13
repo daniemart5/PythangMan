9f1d3c58424b901ef9c1bad86fff929f1bd1935d
daniemart <github email address>
2019-11-13 12:45:51 -0800

    nexted loop practice

---
+++ forLoops.py
@@ -0,0 +1,11 @@
+# iterate over items in a collection:
+
+# for item in ["Python", "Ruby", "CSS", "HTML", "JavaScript "]:
+#     print(item)
+
+prices = [10, 20, 30]
+total = 0
+for price in prices:
+    total += price
+print(f"Total: {total}")
+
---
+++ nestedLoop.py
@@ -0,0 +1,26 @@
+# for x in range(4):
+#     for y in range(3):
+#         print(f"({x}, {y})")
+
+# solved using one loop
+# numbers = [5,2,5,2,2]
+# for number in numbers:
+#         print ('x' * number)
+
+# nested loop solution
+# numbers = [5,2,5,2,2]
+# for number in numbers:
+#     output = ''
+#     for count in range(number):
+#         output += 'x'
+#     print(output)
+
+# nested loop solution
+numbers = [1,1,1,5]
+for number in numbers:
+    output = ''
+    for count in range(number):
+        output += 'x'
+    print(output)
+
+ 
