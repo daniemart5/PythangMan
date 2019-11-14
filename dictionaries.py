8753355fe770fabfe0a3cec314316648d1707fb3
daniemart <github email address>
2019-11-14 15:54:00 -0800

    went over basic functions in python to understand syntax

---
+++ dictionaries.py
@@ -0,0 +1,40 @@
+# customer = { 
+# "Name":"John Smith",
+# "Email":"john@gmail.com",
+# "Phone":1234567890,
+# "is_verified":True
+# }
+# print(customer["Name"])
+
+#dictionary keys should be unique
+
+# phone = input("Phone: ")
+
+# digits = {
+#     "1":"One",
+#     "2":"Two",
+#     "3":"Three",
+#     "4":"Four",
+#     "5":"Five",
+#     "6":"Six",
+#     "7":"Seven",
+#     "8":"Eight",
+#     "9":"Nine "
+# }
+# output = " "
+# for ch in phone:
+#     output += digits.get(ch,"!") + " "
+# print(output)
+
+# message =  input(">")
+# words = message.split(" ")
+
+# emojis = {
+#     ":)":"😀",
+#     ":(":"😞"
+# }
+# output = " "
+# for word in words:
+#     output += emojis.get(word,word) + " "
+
+# print(output)
---
+++ functions.py
@@ -0,0 +1,16 @@
+#python function
+
+
+def greet_user(name):        
+    print(f"Hi there {name}")
+
+
+#its best to have two line breaks between functions in python
+print("Start")
+greet_user("Danie Martinoli")
+greet_user("Nick Martinoli")
+greet_user("Bill Martinoli")
+print("Finish")
+
+#parameter is a place holder
+#argument is an actual piece of info 
